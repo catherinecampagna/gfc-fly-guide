e33c7fe543537975434d40cc02db4c81932d71a6
“Catherine <catherine.campagna@gmail.com>
2023-03-30 10:21:13 -0400

    client and server initialization

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@
 // import the needed node_modules.
 const express = require("express");
 const morgan = require("morgan");
-const { stock, customers } = require("./data/inventory");
 const port = 8888;
 
 
@@ -13,9 +12,13 @@ const app = express()
 
   // This will give us will log more info to the console. see https://www.npmjs.com/package/morgan
   app.use(morgan("tiny"))
-  app..use(express.json())
+  app.use(express.json())
 
-  app..get("*", (req, res) => {
+  app.get("/test", (req, res) => {
+    res.status(200).json({itWorked: true})
+  })
+
+  app.get("*", (req, res) => {
     res.status(404).json({
       status: 404,
       message: "This is obviously not what you are looking for.",
@@ -23,5 +26,4 @@ const app = express()
   })
 
 
-  // Node spins up our server and sets it to listen on port 8000.
-  .listen(8000, () => console.log(`Listening on port 8000`));
+  .listen(port, () => console.log(`Listening on port ${port}`));
